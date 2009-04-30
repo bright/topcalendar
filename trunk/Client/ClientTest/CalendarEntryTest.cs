c708ad0acf5bc188cd7905e26e6db63e8b1d3f68
adamrbar <adamrbar@1e76c072-2ff6-11de-9cad-a3574e5575ef>
2009-04-30 19:20:59 +0000

    poczatek

---
+++ trunk/Client/ClientTest/CalendarEntryTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClientApp;
+
+namespace ClientTest
+{
+    /// <summary>
+    /// Summary description for CalendarEntryTest
+    /// </summary>
+    [TestClass]
+    public class CalendarEntryTest
+    {
+        private CalendarEntry sut;
+
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+
+        // Use TestInitialize to run code before running each test 
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //sut = new CalendarEntry();
+        }
+
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+
+        [TestMethod]
+        public void TitleIsSavedProperly()
+        {
+            string title = "test";
+            sut = new CalendarEntry(title);
+            Assert.IsTrue(sut.Title == title);
+        }
+    }
+}
--- trunk/Client/ClientTest/LocalServerTest.cs
+++ trunk/Client/ClientTest/LocalServerTest.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClientApp;
 
 namespace ClientTest
 {
@@ -12,35 +13,8 @@ namespace ClientTest
     [TestClass]
     public class LocalServerTest
     {
-        public LocalServerTest()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
+        private LocalServer sut;
 
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
         // Use ClassInitialize to run code before running the first test in the class
         // [ClassInitialize()]
         // public static void MyClassInitialize(TestContext testContext) { }
@@ -48,23 +22,56 @@ namespace ClientTest
         // Use ClassCleanup to run code after all tests in a class have run
         // [ClassCleanup()]
         // public static void MyClassCleanup() { }
-        //
+        
         // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            sut = new LocalServer();
+        }
+        
         // Use TestCleanup to run code after each test has run
         // [TestCleanup()]
         // public void MyTestCleanup() { }
         //
-        #endregion
 
         [TestMethod]
-        public void TestMethod1()
+        public void NewServerShouldHaveNoEntries()
         {
-            //
-            // TODO: Add test logic	here
-            //
+            Assert.IsTrue(sut.Count == 0);
         }
+
+        [TestMethod]
+        public void NewItemCanBeAddedAndCounterIncreases()
+        {
+            sut.Add(new CalendarEntry());
+            Assert.IsTrue(sut.Count == 1);
+        }
+
+        [TestMethod]
+        public void NoEntryCanBeFetchedFromEmptyServer()
+        {
+            var data = from x in sut select x;
+            Assert.IsTrue(data.Count<CalendarEntry>() == 0);
+        }
+
+        [TestMethod]
+        public void IfOneEntryIsAddedOneCanBeFetched()
+        {
+            sut.Add(new CalendarEntry());
+            var data = from x in sut select x;
+            Assert.IsTrue(data.Count<CalendarEntry>() == 1);
+        }
+
+        [TestMethod]
+        public void IfEntryIsAddedItCanFetched()
+        {
+            string title = "test";
+            sut.Add(new CalendarEntry(title));
+            var data = from x in sut where x.Title == title select x;
+            Assert.IsTrue(data.Count<CalendarEntry>() == 1);
+            Assert.IsTrue(data.First<CalendarEntry>().Title == title);
+        }
+
     }
 }
