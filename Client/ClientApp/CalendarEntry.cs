f4198dc22752051a81a8da1c48a4d14d4fc4d7b9
adamrbar <adamrbar@1e76c072-2ff6-11de-9cad-a3574e5575ef>
2009-04-30 19:20:59 +0000

    poczatek

---
+++ Client/ClientApp/CalendarEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClientApp
+{
+    public class CalendarEntry
+    {
+        public string Title { get; set; }
+
+        public CalendarEntry() { }
+
+        /**
+         * Nic ciekawego, na razie ustawia tylko tytul
+         */
+        public CalendarEntry(string title)
+        {
+            Title = title;
+        }
+    }
+}
--- Client/ClientApp/LocalServer.cs
+++ Client/ClientApp/LocalServer.cs
@@ -5,7 +5,49 @@ using System.Text;
 
 namespace ClientApp
 {
-    public class LocalServer
+    public class LocalServer : IEnumerable<CalendarEntry>
     {
+        private List<CalendarEntry> data = new List<CalendarEntry>();
+
+        /**
+         * Daje dostep tylko do odczytu do licznika listy
+         */
+        public int Count
+        {
+            get
+            {
+                return data.Count;
+            }
+        }
+
+        /**
+         * Dodaje wydarzenie do listy
+         */
+        public void Add(CalendarEntry e)
+        {
+            data.Add(e);
+        }
+
+        /**
+         * Implementujemy interfejsik zeby mozna bylo pytac nasz "serwer" przez LINQ,
+         * jednoczesnie nie odkrywajac na zewnatrz calej listy
+         */
+        #region IEnumerable<CalendarEntry> Members
+
+        IEnumerator<CalendarEntry> IEnumerable<CalendarEntry>.GetEnumerator()
+        {
+            return data.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return data.GetEnumerator();
+        }
+
+        #endregion
     }
 }
