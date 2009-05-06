ae62dbfaa9379eb011b5eeffb1dcf847a117a5ba
Piotr.Mionskowski <Piotr.Mionskowski@1e76c072-2ff6-11de-9cad-a3574e5575ef>
2009-05-06 10:39:54 +0000

    jakies proste wyświetlanie miesiąca troche blędów ;p

---
+++ Client/ClientUI/DayControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ClientApp;
+
+namespace ClientUI
+{
+    /// <summary>
+    /// Interaction logic for DayControl.xaml
+    /// </summary>
+    public partial class DayControl : UserControl
+    {
+        public DateTime Date { get; set; }
+
+        public List<CalendarEntry> EntryList { get; set; }
+
+        public DayControl()
+        {
+            Date = DateTime.Now;
+            InitializeComponent();
+        }
+
+        public DayControl(DateTime dt) {
+            Date = dt;
+            InitializeComponent();
+        }
+    }
+}
