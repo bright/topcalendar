5ab44c70c03926ff6b1a01a377cf1a707b180093
michal.lukasiewicz <michal.lukasiewicz@1e76c072-2ff6-11de-9cad-a3574e5575ef>
2009-05-07 22:46:09 +0000

    Uproszczone nazewnictwo i wyrzuczony na razie niepotrzebny interfejs (IDataServiceBroker.cs)

---
+++ trunk/Client/ClientApp/Ninject/DIFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject.Core;
+
+namespace ClientApp.Ninject
+{
+    /// <summary>
+    /// Klasa przykrywajaca dzialanie Ninjecta.
+    /// Dzieki temu w calej aplikacji tworzenie obiektow odbywa sie
+    /// za pomoca interfejsu niezaleznego od Ninjecta. 
+    /// Mozna dzieki temu np. dowolnie zmieniac framework IoC.
+    /// </summary>
+    public static class DIFactory 
+    {
+        // glowny modul Ninjecta
+        private static IKernel kernel;
+
+        public static T Resolve<T>()
+        {
+
+            if (kernel == null)
+            {
+                CreateKernel();
+            }
+            return kernel.Get<T>();
+
+        }
+
+        private static void CreateKernel()
+        {  
+            IModule module = new DIModule();
+            kernel = new StandardKernel(module);
+
+        }
+
+    }
+}
