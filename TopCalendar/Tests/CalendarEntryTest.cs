c65b158e837af93f9e074f372d467a6eb971ce39
adamrbar <adamrbar@1e76c072-2ff6-11de-9cad-a3574e5575ef>
2009-06-20 11:47:28 +0000

    baza danych ver. 0.1

--- TopCalendar/Tests/CalendarEntryTest.cs
+++ TopCalendar/Tests/CalendarEntryTest.cs
@@ -6,6 +6,7 @@ using Rhino.Mocks;
 using Rhino.Mocks.Constraints;
 using ClientApp;
 using ServerLib;
+using ServerLib.Data;
 
 namespace ClientTest
 {
@@ -51,6 +52,30 @@ namespace ClientTest
             sut = new BaseCalendarEntry(null,expected);
 
             Assert.AreEqual(0, DateTime.Compare(expected, sut.DateTime));
+        }
+
+        [Test]
+        public void ConversionToDbEventIsValid()
+        {
+            sut = new BaseCalendarEntry("title", DateTime.Now);
+
+            DbEntry db = sut;
+
+            Assert.AreEqual(sut.Title, db.Title);
+            Assert.AreEqual(sut.DateTime, db.DateFrom);
+            Assert.AreEqual(sut.Id, db.Id);
+        }
+
+        [Test]
+        public void ConversionFromDbEventIsValid()
+        {
+            DbEntry db = DbEntry.CreateDbEntry(5, "title", DateTime.Now, 6);
+
+            sut = db;
+
+            Assert.AreEqual(sut.Title, db.Title);
+            Assert.AreEqual(sut.DateTime, db.DateFrom);
+            Assert.AreEqual(sut.Id, db.Id);
         }
     }
 }
