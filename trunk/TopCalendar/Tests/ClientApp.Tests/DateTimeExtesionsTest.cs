63c769c79353525dd4380981eb35c191fcbbd6d8
Piotr.Mionskowski <Piotr.Mionskowski@1e76c072-2ff6-11de-9cad-a3574e5575ef>
2009-10-22 19:41:56 +0000

    zagubione projekty

---
+++ trunk/TopCalendar/Tests/ClientApp.Tests/DateTimeExtesionsTest.cs
@@ -0,0 +1,51 @@
+﻿using ClientApp.DateTimeExtensions;
+using NUnit.Framework;
+using System;
+
+namespace ClientTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for DateTimeExtesionsTest and is intended
+    ///to contain all DateTimeExtesionsTest Unit Tests
+    ///</summary>
+    [TestFixture]
+    public class DateTimeExtesionsTest
+    {
+
+
+        /// <summary>
+        ///A test for NextMonth
+        ///</summary>
+        [Test]
+        public void NextMonth()
+        {
+            DateTime dt = new DateTime(1986, 11, 11);
+            DateTime expected = new DateTime(1986, 12, 1);
+            DateTime actual = dt.NextMonth();
+            Assert.AreEqual(expected, actual);            
+        }
+        [Test]
+        public void NextMonthDecember() {
+            DateTime dt = new DateTime(1986, 12, 1);
+            DateTime expected = new DateTime(1987, 1, 1);
+            DateTime actual = dt.NextMonth();
+            Assert.AreEqual(expected, actual);            
+        }
+        [Test]
+        public void PrevMonth() {
+            DateTime dt = new DateTime(1986, 11, 1);
+            DateTime expected = new DateTime(1986, 10, 1);
+            DateTime actual = dt.PrevMonth();
+            Assert.AreEqual(expected, actual);                  
+        }
+        [Test]
+        public void PrevMonthJanuary() {
+            DateTime dt = new DateTime(2008, 1, 10);
+            DateTime expected = new DateTime(2007, 12, 1);
+            DateTime actual = dt.PrevMonth();
+            Assert.AreEqual(expected, actual);                          
+        }
+    }
+}
---
+++ trunk/TopCalendar/Tests/ServerLib.Tests/CalendarServer_Fixture.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using NUnit.Framework;
+using Rhino.Mocks;
+using ServerLib.Domain;
+
+namespace ServerLib.Tests
+{
+    [TestFixture]
+    public class CalendarServer_Fixture
+    {
+        private readonly MockRepository _mocks = new MockRepository();
+        private IBaseCalendarEntryRepository _baseCalendarEntryRepositoryMock;
+        private CalendarServer _sut;
+
+        #region Setup/Teardown
+
+        private BaseCalendarEntry _testBaseCalendarEntry =
+            new BaseCalendarEntry
+                {
+                    Title = "Test Title",
+                    Desc = "Test description",
+                    DateTime = new DateTime(2009,01,01)
+                };
+
+        [SetUp]
+        public void SetupContext()
+        {
+            _baseCalendarEntryRepositoryMock = _mocks.DynamicMock<IBaseCalendarEntryRepository>();
+            _sut = new CalendarServer(_baseCalendarEntryRepositoryMock);
+        }
+
+        #endregion
+
+        [Test]
+        public void When_adding_new_element_repository_add_method_should_be_fired()
+        {
+            using (_mocks.Record())
+            {
+                Expect.Call(() => _baseCalendarEntryRepositoryMock.Add(null)).IgnoreArguments();
+            }
+            using (_mocks.Playback())
+            {
+                _sut.Add(_testBaseCalendarEntry);
+            }
+        }
+
+        [Test]
+        public void When_updating_existing_element_repository_update_method_should_be_fired()
+        {
+            using (_mocks.Record())
+            {
+                Expect.Call(() => _baseCalendarEntryRepositoryMock.Update(null)).IgnoreArguments();
+            }
+            using (_mocks.Playback())
+            {
+                _sut.EntryEdited(_testBaseCalendarEntry);
+            }
+        }
+
+        [Test]
+        public void When_removing_existing_element_repository_remove_method_should_be_fired()
+        {
+            using (_mocks.Record())
+            {
+                Expect.Call(() => _baseCalendarEntryRepositoryMock.Remove(null)).IgnoreArguments();
+            }
+            using (_mocks.Playback())
+            {
+                _sut.Remove(_testBaseCalendarEntry);
+            }
+        }
+    }
+}
