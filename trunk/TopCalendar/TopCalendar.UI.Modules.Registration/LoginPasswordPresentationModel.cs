2921669a56bcf28bdc7532de6255de4ad5ea95e6
Piotr.Mionskowski <Piotr.Mionskowski@1e76c072-2ff6-11de-9cad-a3574e5575ef>
2009-10-31 14:46:27 +0000

    dodane klasy bazowe, do presentation modelow, i zmieniony registrationmodule

--- trunk/TopCalendar/TopCalendar.UI.Modules.Registration/LoginPasswordPresentationModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TopCalendar.UI.Modules.Registration
-{
-    public class LoginPasswordPresentationModel
-    {
-        public string Login { get; set; }
-        public string Password { get; set; }
-    }
-}
