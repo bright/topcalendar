14b8239cc145ae610b8a2b977a621d51419fe22f
Piotr.Mionskowski <Piotr.Mionskowski@1e76c072-2ff6-11de-9cad-a3574e5575ef>
2009-12-22 20:19:55 +0000

    dalszy ciag weekviewera jeszcze nic sie niestety nie wyswietla

--- trunk/TopCalendar/TopCalendar.UI.Modules.MonthViewer/Services/MonthTaskLoader.cs
+++ trunk/TopCalendar/TopCalendar.UI.Modules.MonthViewer/Services/MonthTaskLoader.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.ObjectModel;
 using System.Linq;
-using AutoMapper;
 using TopCalendar.Client.Connector;
 using TopCalendar.UI.Modules.MonthViewer.Model;
 using TopCalendar.Utility;
--- trunk/TopCalendar/TopCalendar.UI.Modules.WeekViewer/WeekTaskLoader.cs
+++ trunk/TopCalendar/TopCalendar.UI.Modules.WeekViewer/WeekTaskLoader.cs
@@ -1,6 +1,51 @@
+using System;
+using System.Collections.ObjectModel;
+using TopCalendar.Client.Connector;
+
+using TopCalendar.Utility.UI;
+
 namespace TopCalendar.UI.Modules.WeekViewer
 {
 	public class WeekTaskLoader : IWeekTaskLoader
 	{
+		private readonly ITaskRepository _taskRepository;
+		private int DaysInWeek = 7;
+		private int HoursInDay = 24;
+
+		public WeekTaskLoader(ITaskRepository taskRepository)
+		{
+			_taskRepository = taskRepository;
+		}
+
+		public ObservableCollection<ObservableCollection<HourTaskList>> GetTasksForWeek(DateTime time)
+		{
+			var result = GetResultList();
+			return result;
+		}
+
+		private ObservableCollection<ObservableCollection<HourTaskList>> GetResultList()
+		{
+			var result = new ObservableCollection<ObservableCollection<HourTaskList>>();
+			for (int i = 0; i < DaysInWeek; ++i)
+			{
+				var hourTaskLists = new ObservableCollection<HourTaskList>();
+				result.Add(hourTaskLists);
+				for (var h = 0; h < HoursInDay; ++h)
+					hourTaskLists.Add(new HourTaskList(DateTime.Now));
+			}
+			return result;
+		}
+	}
+
+	public class HourTaskList : NotifyPropertyChanged
+	{
+		private readonly DateTime _time;
+
+		public HourTaskList(DateTime time)
+		{
+			_time = new DateTime(time.Year,time.Month, time.Day, time.Hour, 0,0);
+		}
+
+		public DateTime Time { get { return _time; } }
 	}
 }
