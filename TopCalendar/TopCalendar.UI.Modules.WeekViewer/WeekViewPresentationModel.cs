afb4d37048f612a6ee0592514cf6949aed81c48f
Piotr.Mionskowski <Piotr.Mionskowski@1e76c072-2ff6-11de-9cad-a3574e5575ef>
2009-12-22 20:19:55 +0000

    dalszy ciag weekviewera jeszcze nic sie niestety nie wyswietla

--- TopCalendar/TopCalendar.UI.Modules.WeekViewer/WeekViewPresentationModel.cs
+++ TopCalendar/TopCalendar.UI.Modules.WeekViewer/WeekViewPresentationModel.cs
@@ -1,21 +1,86 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.Practices.Composite.Events;
+using Microsoft.Practices.Composite.Presentation;
+using Microsoft.Practices.Composite.Presentation.Commands;
 using TopCalendar.Utility.UI;
+
 
 namespace TopCalendar.UI.Modules.WeekViewer
 {
 	public class WeekViewPresentationModel : PresentationModelFor<IWeekView>
 	{
-		public WeekViewPresentationModel(IWeekView view, IWeekTaskLoader taskLoader) : base(view)
-		{
+		private readonly IWeekTaskLoader _taskLoader;
+		private readonly IEventAggregator _eventAggregator;
+		
 
+		public WeekViewPresentationModel(IWeekView view, IWeekTaskLoader taskLoader, IEventAggregator eventAggregator): base(view)
+		{
+			_taskLoader = taskLoader;
+			_taskLoader = taskLoader;
+			CurrentWeek = DateTime.Now;
+			_eventAggregator = eventAggregator;
+			GoToNextWeek = new DelegateCommand<object>(GoToNextMonthImpl);
+			GoToPreviousWeek = new DelegateCommand<object>(GoToPreviousWeekImpl);
+			InitializeTasks();
 			_view.ViewModel = this;
 		}
+
+		private void InitializeTasks()
+		{
+			Tasks = _taskLoader.GetTasksForWeek(CurrentWeek);
+		}
+
+		private void GoToPreviousWeekImpl(object obj)
+		{
+			CurrentWeek = CurrentWeek.AddDays(-7);
+			UpdateTasksList(CurrentWeek);
+		}
+
+		private void UpdateTasksList(DateTime time)
+		{
+			var tasks = _taskLoader.GetTasksForWeek(CurrentWeek);
+			for(int i = 0; i < Tasks.Count; ++i)
+				for(int j = 0; j <Tasks[i].Count; ++j)
+				{
+					Tasks[i][j] = tasks[i][j];
+				}
+		}
+
+		private void GoToNextMonthImpl(object obj)
+		{
+			CurrentWeek = CurrentWeek.AddDays(7);
+			UpdateTasksList(CurrentWeek);
+		}
+
+		public DateTime CurrentWeek
+		{
+			get; private set;
+		}
+
+		public ICommand GoToNextWeek
+		{
+			get; private set;
+		}
+
+		public ICommand GoToPreviousWeek
+		{
+			get;
+			private set;
+		}
+
+		public ObservableCollection<ObservableCollection<HourTaskList>> Tasks { get; private set; }
 	}
 
 	public interface IWeekTaskLoader
 	{
+		ObservableCollection<ObservableCollection<HourTaskList>> GetTasksForWeek(DateTime time);
 	}
 
-	public interface IWeekView : IViewForModel<IWeekView,WeekViewPresentationModel>
+	public interface IWeekView : IViewForModel<IWeekView, WeekViewPresentationModel>
 	{
 	}
+
+
 }
